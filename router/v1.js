c5ca4ddddde3047f8d4b439b743b44d48f596dd1
osemu <vincentaimiosior@example.com>
2026-03-10 17:02:53 +0100

    update

--- router/v1.js
+++ router/v1.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const {newInferenceJob} = require('../controllers/controlPlane/inferenceController');
+const {newJob} = require('../controllers/controlPlane/apiGateway/task');
 const authenticateClient = require('../middleware/auth');
 const rateLimiter = require('../middleware/rateLimiter');
 
 
-router.route('/new-inference-job')
-    .get(authenticateClient, rateLimiter, newInferenceJob);
+router.route('/new-job')
+    .post(authenticateClient, rateLimiter, newJob);
+
+router.route('/job/result/:JOB_ID')
+    .post(authenticateClient, rateLimiter, newInferenceJob);
 
 module.exports = router;
